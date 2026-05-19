d239bffae3697241e051500edc7d0308d7fc24aa
PavaniPolavarapu8 <pavanipolavarapu4@gmail.com>
2026-05-19 19:04:20 +0530

    Update config.php
    Updated

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ session_start();
 
 $host = 'localhost';
 $db_user = 'root';
-$db_password = '';
+$db_password = 'Your_DB_Password';
 $db_name = 'healthcare_ai';
 
 $conn = new mysqli($host, $db_user, $db_password, $db_name);
